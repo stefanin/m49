8247e23700bbf1b83f497ccd8c08ec7ee9e30cfe
stefanin <stefano.cornelli@gmail.com>
2020-07-05 10:28:30 +0000

    0.5.4 build 20.7.5

--- public/js/m49.js
+++ public/js/m49.js
@@ -12,13 +12,13 @@ function wait(contenitore)
     switch(Math.floor(Math.random() * 3)) {
         case 0:
             var messaggio = "Wait .....";
-        break; 
+        break;
         case 1:
             var messaggio = "Aspetta .....";
-        break; 
+        break;
         case 2:
             var messaggio = "Carico .....";
-        break; 
+        break;
     }
 
     $(contenitore).html("<br><br><br><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+messaggio+"</b>");
@@ -34,4 +34,27 @@ function GetP(contenitore, url)
 
 function mmenu(contenitore){
     alert('eccolo');
+}
+// 2020 07 05 convert date ISO format in gg/mm/hhhh O:M:s
+function FormattaData(iddata,data){
+                    var dd = new Date(data);
+                    document.getElementById(iddata).innerHTML = dd.toLocaleDateString('en-GB')+" "+dd.toLocaleTimeString('en-GB');
+
+}
+
+//2020 07 05 format the status code
+function FormattaStatus(idstatus,status){ 
+    if(status=="0"){
+        document.getElementById(idstatus).innerHTML = status.replace("0","<span class=\"badge badge-danger\">Danger</span>");
+    }
+    if(status=="1"){
+        document.getElementById(idstatus).innerHTML = status.replace("1","<span class=\"badge badge-warning\">Warning</span>");
+    }
+    if(status=="2"){
+        document.getElementById(idstatus).innerHTML = status.replace("2","<span class=\"badge badge-info\">Man.</span>");
+    }
+    if(status=="3"){
+        document.getElementById(idstatus).innerHTML = status.replace("3","<span class=\"badge badge-success\">Good</span>");
+    }
+
 }
